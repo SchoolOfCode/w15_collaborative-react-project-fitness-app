97416fd40d038e7a29699a1545db11c78d35825e
codewurld <sholaq@gmail.com>
2021-07-03 15:39:23 +0100

    added some components

---
+++ flexercise/src/components/1_BgImage/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// const backgroundImg = "https://images.unsplash.com/photo-1470468969717-61d5d54fd036?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1283&q=80"
+
+function BackgroundImage() {
+    return (
+        <div>
+            <h1>Pretend Like It Never Happened!</h1>
+            <img src="shorturl.at/ijuOZ" alt="working out" />
+            <img
+                src="https://scontent-lcy1-1.xx.fbcdn.net/v/t1.6435-9/199969082_10166027799320151_7150298100143503879_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=zbf7J8A8zswAX_wvHJs&tn=BJEcE2LDxKTlylcE&_nc_ht=scontent-lcy1-1.xx&oh=91eed009dbd9eb770e8a714a90d2915e&oe=60E46D2E"
+                alt="Cartoon of Becki"
+            />
+        </div>
+    )
+}
+
+export default BackgroundImage;
