b60f918e83f60559a8ab87f9dcfb50e3baa56968
Becki Floyd <beckifloyd@gmail.com>
2021-07-03 19:39:08 +0100

    Heading H1 Component

---
+++ flexercise/src/components/6_HeadingText/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function HeadingText({heading}) {
+    return (
+        <div>
+            <h1>{heading}</h1>
+        </div>
+    )
+}
+
+export default HeadingText;
+
+// import HeadingText from "../6_HeadingText";
+// <HeadingText heading="Exercise Challenge Overview"/>
+// <HeadingText heading="Instructions"/>
