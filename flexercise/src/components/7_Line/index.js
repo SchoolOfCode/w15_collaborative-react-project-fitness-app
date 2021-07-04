c6d44c846d93070ad75640c7c259ae88e5785967
Becki Floyd <beckifloyd@gmail.com>
2021-07-04 08:47:49 +0100

    7_Line completed

---
+++ flexercise/src/components/7_Line/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Line() {
+    return (
+        <div className="thinline"> </div>
+    )
+}
+
+export default Line;
+
+/* <Line /> */
--- flexercise/src/components/App/App.js
+++ flexercise/src/components/App/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './App.css';
 import BackgroundImage from '../1_BgImage';
-
+import Line from "../7_Line";
 
 function App() {
   return (
     <div className="App">
-      <BackgroundImage />
+    
       
     </div>
   );
