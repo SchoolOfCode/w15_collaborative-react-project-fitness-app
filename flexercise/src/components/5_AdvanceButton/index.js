2edb45598c9d43cdb8f99752313a0d2ab712c448
codewurld <sholaq@gmail.com>
2021-07-05 12:09:55 +0100

    redid the advance button

---
+++ flexercise/src/components/5_AdvanceButton/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AdvanceButton() {
+    return (
+        <button className="advance-button">
+            {text}
+        </button>
+    )
+}
+
+export default AdvanceButton;
