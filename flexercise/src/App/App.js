5afc9b20aeb01282b73f1c4f9d9e02e3d9a42555
Becki Floyd <beckifloyd@gmail.com>
2021-07-07 22:40:00 +0100

    Timer Added and Hooked up to App

--- flexercise/src/App/App.js
+++ flexercise/src/App/App.js
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import Homepage from "../pages/Homepage/index";
 import Explanation from "../pages/Explanation/index";
 import ExerciseSelection from "../pages/ExerciseSelection";
+import TimerPage from "../pages/TimerPage";
 import "./App.css";
 
 function App() {
   const [currentPage, setCurrentPage] = useState("homepage");
-  const [user, setUser] = useState ({})
-  console.log(user)
+  const [user, setUser] = useState({});
+  console.log(user);
   switch (currentPage) {
     case "homepage":
       return <Homepage onComplete={() => setCurrentPage("explanation")} />;
@@ -19,19 +20,22 @@ function App() {
       return (
         <ExerciseSelection
           onComplete={(name, exercise) => {
-            setUser(
-              {
-                ...user, 
-                name, exercise
-              }
-            )
-            // setCurrentPage("exerciseselection")
-           
-            
-            }
-          }
-          
+            setUser({
+              ...user,
+              name,
+              exercise,
+            });
+
+            setCurrentPage("timer");
+          }}
         />
+      );
+    case "timer":
+      return (
+        <TimerPage
+          user={user}
+          onComplete={() => setCurrentPage("inputpage")}
+        ></TimerPage>
       );
   }
 }
