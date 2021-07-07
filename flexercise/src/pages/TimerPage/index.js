3ab0eb0363a14a455893025531e5aacfbebd3103
Becki Floyd <beckifloyd@gmail.com>
2021-07-07 22:47:25 +0100

    Video prop added

--- flexercise/src/pages/TimerPage/index.js
+++ flexercise/src/pages/TimerPage/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Timer from "../../components/12_TimerAndStartButton";
 import Logo from "../../components/2_Logo";
-import Video from "../../components/11_Video";
+import YoutubeEmbed from "../../components/11_Video";
 
 const TimerPage = ({ onComplete, user }) => {
   return (
     <div className="timer-page">
       <Logo />
-      <Video />
+      <YoutubeEmbed embedId="9OfycnUL0h8?ecver=1&amp" />
       {/* change to 120 past testing */}
       <Timer startTime={12} onComplete={onComplete} />
     </div>
