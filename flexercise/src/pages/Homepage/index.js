4646f89cade038a931ab72c2ff1277ac39c15b9d
nass84 <80827418+nass84@users.noreply.github.com>
2021-07-10 09:22:53 +0100

    Update index.js

--- flexercise/src/pages/Homepage/index.js
+++ flexercise/src/pages/Homepage/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import BackgroundImage from "../../components/1_BackgroundImage/index"
 import LogoImage from "../../components/2_Logo";
 
+// This page is the Home Page, the onComplete prop changes to the next page on press of a button 
+
 const Homepage = ({ onComplete }) => {
   return (
     <div className="page-div">     
