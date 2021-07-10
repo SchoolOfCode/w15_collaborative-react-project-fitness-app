65772907bc3616a24511b046c9dfaa9a6659a615
nass84 <80827418+nass84@users.noreply.github.com>
2021-07-10 12:21:44 +0100

    Comments added to explain code

--- flexercise/src/components/0_SmallLogo/index.js
+++ flexercise/src/components/0_SmallLogo/index.js
@@ -1,5 +1,5 @@
 // Small Flexercise Logo to be added on several pages see component tree
-/* The logo is transparent, the background colour for pages is 232323 
+// The logo is transparent, the background colour for pages is 232323 
 
 
 import React from "react";
