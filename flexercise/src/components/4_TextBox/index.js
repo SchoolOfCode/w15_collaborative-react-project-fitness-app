48872f74c4d684fd6172bf9ecc06d40ae4f62b09
nass84 <80827418+nass84@users.noreply.github.com>
2021-07-10 12:25:31 +0100

    Comments added to explain code

--- flexercise/src/components/4_TextBox/index.js
+++ flexercise/src/components/4_TextBox/index.js
@@ -1,10 +1,9 @@
 // A component to add text to pages
-/* To add this write -  import TextBox from "../4_TextBox";
-Examples of what to write in the return function: 
-Exercise Selection Page 
-<TextBox className="" text="You are about to complete a short base test which will be used to create a daily workout challenge for you."/>
-<TextBox className="" text="Enter UserName"/>
-<TextBox className="" text="Pick Exercise"/> */
+
+// Examples of what to write in the return function: 
+// Exercise Selection Page 
+// <TextBox className="" text="You are about to complete a short base test which will be used to create a daily workout challenge for you."/>
+// <TextBox className="" text="Pick Exercise"/> */
 
 import React from "react";
 
