11b2df328e23a46c47301cae029f4ee012f2084d
nass84 <80827418+nass84@users.noreply.github.com>
2021-07-10 10:35:36 +0100

    Update index.js

--- flexercise/src/pages/Explanation/index.js
+++ flexercise/src/pages/Explanation/index.js
@@ -2,17 +2,22 @@ import React from "react";
 import TextBox from "../../components/4_TextBox";
 import Line from "../../components/7_Line";
 
+// This page explains to the user what they will be doing. 
 const Explanation = ({ onComplete }) => {
   return (
     <div className="page-div">
       <div className="explanation-background">
+// Aria- Hidden removes decorative items from the accessibility tree. This can improve the experience for assistive technology users.
         <h1><i class="fa fa-list" aria-hidden="true"></i> Exercise Challenge Overview</h1>
+// This inserts the text box component, the text you set can change the wording in the box
         <TextBox
           className="explanation-text"
           text="You are about to complete a short base test which will be used to create a daily workout challenge for you."
         />
+// Decorative Line component
         <Line />
         <h1><i class="fa fa-list" aria-hidden="true"></i> Instructions</h1>
+// Text Boxes will include icon components in the next version of the app which is why they have been split up
         <TextBox className="explanation-text" text="Enter Username" />
         <TextBox className="explanation-text" text="Pick Exercise" />
         <TextBox
@@ -32,6 +37,7 @@ const Explanation = ({ onComplete }) => {
           text="Your score will be entered on the daily leaderboard so you can challenge other people "
         />
       </div>
+// This button will take the user to the next page
       <button className="my-button" onClick={onComplete}>
         Continue
       </button>
