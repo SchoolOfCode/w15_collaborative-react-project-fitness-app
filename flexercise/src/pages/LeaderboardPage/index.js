3586c20ba279faf105f8dd0e147eea5ca4a156c5
nass84 <80827418+nass84@users.noreply.github.com>
2021-07-10 12:07:40 +0100

    Add comments to explain the code

--- flexercise/src/pages/LeaderboardPage/index.js
+++ flexercise/src/pages/LeaderboardPage/index.js
@@ -1,11 +1,13 @@
 import Leaderboard from "../../components/14_Leaderboard";
 import HeadingText from "../../components/6_HeadingText";
 
+// Displays the leaderboard page on the screen
 const LeaderboardPage = ({ onComplete }) => {
   return (
     <div className="page-div">
       <HeadingText heading="Leaderboard" />
       <Leaderboard />
+// On click of button go back to exercise selection page
       <button className="my-button" onClick={onComplete}>
         Finished
       </button>
