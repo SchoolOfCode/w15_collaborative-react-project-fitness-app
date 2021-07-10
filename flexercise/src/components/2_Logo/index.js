13ee08ff5c5bc9eee098ddd577d779876039cdb0
nass84 <80827418+nass84@users.noreply.github.com>
2021-07-10 12:23:50 +0100

    Comments added to explain code

--- flexercise/src/components/2_Logo/index.js
+++ flexercise/src/components/2_Logo/index.js
@@ -1,3 +1,5 @@
+// This is the high quality logo seperated into the picture and words
+
 import React from "react";
 import Logo from "../../images/logo.svg";
 
@@ -5,7 +7,9 @@ function LogoImage() {
   return (
     <div className="logo-div">
       <img className="logo-img" src={Logo} alt="logo" />
+// Flexercise picture 
       <h1 id="title-flexercise">FLEXERCISE</h1>
+// Flexercise slogan
       <h3 id="slogan-flexercise">Fitness Made Fun</h3>
     </div>
   );
