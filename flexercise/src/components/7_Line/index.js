22bf02c9027590f06aa15f16b3d1e85431fc2687
nass84 <80827418+nass84@users.noreply.github.com>
2021-07-10 12:29:04 +0100

    Comments added to explain code

--- flexercise/src/components/7_Line/index.js
+++ flexercise/src/components/7_Line/index.js
@@ -1,7 +1,5 @@
 // This makes a thin line on the page. The styling is in index.css
-// To import add to top of screen -  import Line from "../7_Line";
-// Add to return function
-/* <Line /> */
+
 
 import React from "react";
 
