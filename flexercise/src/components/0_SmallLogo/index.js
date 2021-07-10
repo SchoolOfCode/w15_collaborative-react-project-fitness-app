7c701b9cfd650cc077de89617a3087f48a05b554
nass84 <80827418+nass84@users.noreply.github.com>
2021-07-10 12:21:19 +0100

    Update index.js

--- flexercise/src/components/0_SmallLogo/index.js
+++ flexercise/src/components/0_SmallLogo/index.js
@@ -1,6 +1,6 @@
-// Flexercise Logo to be added on several pages see component tree
+// Small Flexercise Logo to be added on several pages see component tree
 /* The logo is transparent, the background colour for pages is 232323 
-// To import to upper components add: <Logo /> */
+
 
 import React from "react";
 import Logo from "../../images/logo.svg";
