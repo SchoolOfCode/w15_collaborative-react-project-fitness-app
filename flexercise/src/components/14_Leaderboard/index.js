a02546e88af729dc266b45aeeca4985c0633df66
nass84 <80827418+nass84@users.noreply.github.com>
2021-07-10 13:47:31 +0100

    Comments added to explain code

--- flexercise/src/components/14_Leaderboard/index.js
+++ flexercise/src/components/14_Leaderboard/index.js
@@ -1,11 +1,17 @@
+// This creates the leaderboard with the information gathered from the start 
+
 import { useEffect, useState, useCallback } from "react";
 
+// Leaderboard information 
 const TOKEN = "60a8e2c18f40bb64ec94690b";
 const URL = `https://www.dreamlo.com/lb/${TOKEN}/json`;
 
+// Creates Leaderboard 
 const Leaderboard = () => {
+  // Sets inital value of state to be an empty object 
   const [result, setResult] = useState({});
 
+// Callback function used to handle data from leaderboard API 
   const updateResults = useCallback(async () => {
     const response = await fetch(URL);
     const data = await response.json();
@@ -13,10 +19,12 @@ const Leaderboard = () => {
     console.log(result);
   }, [result]);
 
+ // Only run when update results happens
   useEffect(() => {
     updateResults();
   }, [updateResults]);
 
+  // displays the leader board on the page
   return (
     <div className="leaderboard">
       <table>
@@ -37,6 +45,7 @@ const Leaderboard = () => {
             );
           })
         ) : (
+ // clean up for fetching API data
           <p>Loading...</p>
         )}
       </table>
