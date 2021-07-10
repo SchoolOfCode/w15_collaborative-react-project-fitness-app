4d4378d4b7322a0285a63fc2782f9502c9c9c97d
nass84 <80827418+nass84@users.noreply.github.com>
2021-07-10 11:09:21 +0100

    Added explanation comments to code

--- flexercise/src/pages/TimerPage/index.js
+++ flexercise/src/pages/TimerPage/index.js
@@ -3,13 +3,16 @@ import Timer from "../../components/12_TimerAndStartButton";
 import SmallLogo from "../../components/0_SmallLogo";
 import YoutubeEmbed from "../../components/11_Video";
 
+// This page shows the video of how to do a Squat and Displays the timer
 const TimerPage = ({ onComplete, user }) => {
   return (
     <div className="page-div">
       <SmallLogo />
       <YoutubeEmbed embedId="9OfycnUL0h8?ecver=1&amp" />
-      {/* change to 120 past testing */}
-      <Timer startTime={1} onComplete={onComplete} />
+   
+// Once Timer has counted down, the page will change to the input page. 
+       {/* change to 120 past testing */}
+      <Timer startTime={10} onComplete={onComplete} />
     </div>
   );
 };
