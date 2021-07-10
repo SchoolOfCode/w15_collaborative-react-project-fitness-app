553c7196e267cef5f32f7dfa8838bb3466e9c71e
nass84 <80827418+nass84@users.noreply.github.com>
2021-07-10 13:42:21 +0100

    Comments added to explain code

--- flexercise/src/components/13_Motivational/index.js
+++ flexercise/src/components/13_Motivational/index.js
@@ -1,13 +1,17 @@
-import React from "react";
+// This component is not used on the MVP as only one motivational page however on second iteration 
+// motivational quote and image will be sourced from an array which will change depending on range of user exercise amount input
 
-// motivational quote and image will be sourced from an array using array methods depending on range of user exercise amount input
+import React from "react";
 
 function MotivationalQuote({ text }) {
   return (
     <div className="motivational">
+//motivational image 
       <div className="motivational-image-container">
         <img className="motivational-image" src={image} />
       </div>
+
+//motivational text 
       <div className="motivation-text-container">
         <p>{text}</p>
       </div>
