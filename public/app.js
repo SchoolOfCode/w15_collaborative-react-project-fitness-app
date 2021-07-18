b12f920b16875841c74b91531b07cba77f0ce65a
null <null>
2021-07-18 10:03:49 +0100

    Updates

--- public/app.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register('/w15_collaborative-react-project-fitness-app/serviceWorker.js', {scope: '/w15_collaborative-react-project-fitness-app/'})
-      .then(registration => {
-          console.log("service worker registered");
-      }).catch(error => {
-          console.log("service worker not registered", error);
-  })
-}
