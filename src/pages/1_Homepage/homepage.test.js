a8b97c62b06522033083c0b97a73537fcf5519a9
gschandan <gsc128@gmail.com>
2021-07-20 23:01:52 +0000

    testing home page completed

--- src/pages/1_Homepage/homepage.test.js
+++ src/pages/1_Homepage/homepage.test.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 import Homepage from "./index"
+import {Router} from "react-router-dom"
+import { createMemoryHistory } from 'history'
+import App from "../../App/App"
 
-describe("Sequentially testing rendering of pages in the linear site", () => {
-  test("Renders homepage button and content on loading", () => {
-    render(
-    <Homepage />
-    );
-    expect(screen.getByTestId("homepage-div")).toBeInTheDocument();
-  });
+afterEach(cleanup);
 
-})
+test("Renders homepage button and content on loading", () => {
+  const history = createMemoryHistory()
+  render(
+    <Router history={history}>
+      <Homepage />
+    </Router>
+  )
+  expect(screen.getByTestId("homepage-div")).toBeInTheDocument();
+  expect(screen.getByRole('button', {class:/my-button/i})).toBeInTheDocument();
+});
 
+test("Clicking the start button takes you to the explanation page", () => {
+  const history = createMemoryHistory()
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  )
+  const button = screen.getByRole('button', {class:/my-button/i})
+  userEvent.click(button, { button: 0 })
+  expect(screen.getByText(/You are about to complete a short/i)).toBeInTheDocument()
+});
